4ddd72efc5e4460010299546fd0f78ddd5dbdb6b
Peter O'Malley <pomalley@physics.ucsb.edu>
2011-03-31 23:21:48 +0000

    Added javadoc comments.

--- src/org/labrad/qubits/channels/Channel.java
+++ src/org/labrad/qubits/channels/Channel.java
@@ -4,6 +4,15 @@ import org.labrad.qubits.Experiment;
 import org.labrad.qubits.FpgaModel;
 import org.labrad.qubits.resources.DacBoard;
 
+
+/**
+ * "Channels represent the various signal generation and measurement capabilities that are needed in a
+ * particular experiment(IQ, Analog or FastBias, for example), and are assigned names by the user."
+ * 
+ * In the {@link Device} class, for example, a channel connects a physical device to an experimental parameter.
+ * 
+ * @author maffoo
+ */
 public interface Channel {
   public String getName();
 
