5f9e56ee5a031bd7448bcf4a108ec1e88b2c4ffb
Daniel Egger <nothing@nothing.com>
2012-05-03 01:26:33 +0000

    Messed around

--- GRAPE/GRAPE.py
+++ GRAPE/GRAPE.py
@@ -24,24 +24,30 @@ from labrad import util
 from twisted.internet import defer, reactor
 from twisted.internet.defer import returnValue
 
+from pyle.dataking import util as datakingUtil
+from pyle import registry
+
 import os
 
+GRAPERunName = 'InterfaceTest'
+EXECUTABLE = './UCSB_GRAPE_CZ.sh'+GRAPERunName
 
-CONTROL_PARAMETERS = {}
+#CONTROL_PARAMETERS = [(<LabRAD name>, <GRAPE name>, <unitTag>)]
 
-TARGET_PARAMETERS = {}
+#TARGET_PARAMETERS = {}
 
+# Function to write the input GRAPE needs
 def writeParameterFile(path, control, target):
     toWrite = []
-    for parameter, alias, unitTag in CONTROL_PARAMETERS:
-        toWrite.append((alias, control[parameter], unitTag))
-    for parameter, alias, unitTage in TARGET_PARAMETERS:
-        toWrite.append((alias, target[parameter], unitTag))
-    f = #<open file for writing>
+    for parameter, grapeName, unitTag in CONTROL_PARAMETERS:
+        toWrite.append((grapeName, control[parameter], unitTag))
+    for parameter, grapeName, unitTage in TARGET_PARAMETERS:
+        toWrite.append((grapeName, target[parameter], unitTag))
+    f = open(path)
     for alias, value, unitTag in toWrite:
-        f.write('<'+alias+'>')
+        f.write('<'+grapeName+'>')
         f.write(makeWriteable(value, unitTag))
-        f.write('</'+alias+'>')
+        f.write('</'+grapeName+'>')
     f.close
     
 def makeWriteable(value, unitTag):
@@ -54,35 +60,38 @@ class GRAPE(LabradServer):
     """Invokes GRAPE algorithm on the local machine"""
     name = "GRAPE"
     
-    @setting(20, session = '*s', returns = '')
-    def session(self, c, session):
-        """Get a registry wrapper for the user's session and keep it in this context"""
-        cxn = self.client
-        reg = registry.RegistryWrapper(cxn, session)
-        c['sample'] = reg        
+#    @setting(20, session = '*s', returns = '')
+#    def session(self, c, session):
+#        """Get a registry wrapper for the user's session and keep it in this context"""
+#        cxn = self.client
+#        reg = registry.RegistryWrapper(cxn, session)
+#        c['sample'] = reg        
         
-    @setting(30, controlIdx = 'i', targetIdx = 'i', returns = '*2v')
-    def controlZ(self, c, controlIdx, targetIdx):
-        """Buids GRAPE control z sequence from """
-        cxn = self.client
-        sample, qubits = pyle.dataking.util.loadQubits(c['sample'])
-        control = qubits[controlIdx]
-        target = qubits[targetIdx]
-        #Write relevant parameters to file
-        writeParameterFile(c['parameterFilename'], control, target)
-        #Invoke GRAPE
-        os.system(<run GRAPE>)
-        #Read GRAPE result from file and parse
-        #Return result
-        return np.array([[0,0],[0,0]])
+#    @setting(30, controlIdx = 'i', targetIdx = 'i', returns = '*2v')
+#    def controlZ(self, c, controlIdx, targetIdx):
+#        """Buids GRAPE control z sequence from """
+#        cxn = self.client
+#        sample, qubits = datakingUtil.loadQubits(c['sample'])
+#        control = qubits[controlIdx]
+#        target = qubits[targetIdx]
+#	# Need to set this up so that it writes two files with usage of Hnl or not!
+#        #Write relevant parameters to file
+#	os.chdir('/home/daniel/UCSB_CZ/')
+#        writeParameterFile('Run1_InputData.dat', control, target)
+#        writeParameterFile('Run2_InputData.dat', control, target)
+#        #Invoke GRAPE
+#        os.system(EXECUTABLE)
+#        #Read GRAPE result from file and parse
+#        #Get result and turn it into a numpy array
+#        return result
     
-    @setting(31, path = 's')
-    def cd(self, c, path):
-        os.chdir(path)
+#    @setting(31, path = 's')
+#    def cd(self, c, path):
+#        os.chdir(path)
     
-    @setting(32, filename='s')
-    def setParameterFileName(self, c, filename):
-        c['parameterFileName'] = filename
+#    @setting(32, filename='s')
+#    def setParameterFileName(self, c, filename):
+#        c['parameterFileName'] = filename
         
     
     
