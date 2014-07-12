927d08caadf40371d2ca0c67b543e1c5d47eacb0
Red Heat <this@is.placeholder.com>
2014-07-12 16:37:57 -0700

    Added exceptions to prevent board from timing out or screwing up.

--- GHzDACs/Cleanup/adc.py
+++ GHzDACs/Cleanup/adc.py
@@ -706,6 +706,11 @@ class ADC_Branch2(ADC):
         
         if len(triggerTable) > 128: # no memory for more than 128 entries
             raise Exception("Trigger table max len = 128")
+        if triggerTable[0][1] < 1:
+            raise Exception("Trigger table row0 rdelay is 0")
+        if triggerTable[0][0] > 1:
+            raise Exception("Trigger table row0 rcount > 1, this is dangerous as repeating this row accounts for the first rdelay")
+        
         rlens = [row[1]<50 for row in triggerTable]
         
         # if there is a spacing of <50 clock cycles between demods then the FIFO gets backed up
