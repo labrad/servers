a0555bd039c1d32d31e468cba04a44fa2b7bb428
Evan Jeffrey <ejeffrey@physics.ucsb.edu>
2015-03-29 18:48:41 -0700

    Refactor .ini handling so that HDF5 files can migrate to internal metadata

--- datavault/backend.py
+++ datavault/backend.py
@@ -1,5 +1,6 @@
 import os
-
+import base64
+import datetime
 from twisted.internet import reactor
 
 try:
@@ -9,22 +10,31 @@ except ImportError, e:
     print e
     print "Numpy not imported.  The DataVault will operate, but will be slower."
     use_numpy = False
+from labrad import types as T
+from . import errors, util
 
-from .errors import BadDataError
-
+TIME_FORMAT = '%Y-%m-%d, %H:%M:%S'
 PRECISION = 12 # digits of precision to use when saving data
 DATA_FORMAT = '%%.%dG' % PRECISION
 FILE_TIMEOUT = 60 # how long to keep datafiles open if not accessed
 DATA_TIMEOUT = 300 # how long to keep data in memory if not accessed
+DATA_URL_PREFIX = 'data:application/labrad;base64,'
+
+def time_to_str(t):
+    return t.strftime(TIME_FORMAT)
+
+def time_from_str(s):
+    return datetime.datetime.strptime(s, TIME_FORMAT)
 
 class SelfClosingFile(object):
     """
     A container for a file object that closes the underlying file handle if not
     accessed within a specified timeout. Call this container to get the file handle.
     """
-    def __init__(self, filename, mode, timeout=FILE_TIMEOUT, touch=True):
-        self.filename = filename
-        self.mode = mode
+    def __init__(self, opener=open, open_args=(), open_kw={}, timeout=FILE_TIMEOUT, touch=True):
+        self.opener = opener
+        self.open_args = open_args
+        self.open_kw = open_kw
         self.timeout = timeout
         self.callbacks = []
         if touch:
@@ -32,7 +42,7 @@ class SelfClosingFile(object):
 
     def __call__(self):
         if not hasattr(self, '_file'):
-            self._file = open(self.filename, self.mode)
+            self._file = self.opener(*self.open_args, **self.open_kw)
             self._fileTimeoutCall = reactor.callLater(self.timeout, self._fileTimeout)
         else:
             self._fileTimeoutCall.reset(self.timeout)
@@ -51,18 +61,183 @@ class SelfClosingFile(object):
     def onClose(self, callback):
         self.callbacks.append(callback)
 
-class CsvListData(object):
+class IniData(object):
+    """
+    Handles dataset metadata stored in INI files.  
+
+    This is used via subclassing mostly out of laziness: this was the
+    easy way to separate it from the code that messes with the acutal
+    data storage so that the data storage can be modified to use HDF5
+    and complex data structures.  Once the HDF5 stuff is finished,
+    this can be changed to use composition rather than inheritance.
+    This provides the load() and save() methods to read and write the
+    INI file as well as accessors for all the metadata attributes.
+    """
+    def load(self):
+        S = util.DVSafeConfigParser()
+        S.read(self.infofile)
+
+        gen = 'General'
+        self.title = S.get(gen, 'Title', raw=True)
+        self.created = time_from_str(S.get(gen, 'Created'))
+        self.accessed = time_from_str(S.get(gen, 'Accessed'))
+        self.modified = time_from_str(S.get(gen, 'Modified'))
+
+        def getInd(i):
+            sec = 'Independent %d' % (i+1)
+            label = S.get(sec, 'Label', raw=True)
+            units = S.get(sec, 'Units', raw=True)
+            return dict(label=label, units=units)
+        count = S.getint(gen, 'Independent')
+        self.independents = [getInd(i) for i in range(count)]
+
+        def getDep(i):
+            sec = 'Dependent %d' % (i+1)
+            label = S.get(sec, 'Label', raw=True)
+            units = S.get(sec, 'Units', raw=True)
+            categ = S.get(sec, 'Category', raw=True)
+            return dict(label=label, units=units, category=categ)
+        count = S.getint(gen, 'Dependent')
+        self.dependents = [getDep(i) for i in range(count)]
+        
+        self.cols = len(self.independents + self.dependents)
+
+        def getPar(i):
+            sec = 'Parameter %d' % (i+1)
+            label = S.get(sec, 'Label', raw=True)
+            raw = S.get(sec, 'Data', raw=True)
+            if raw.startswith(DATA_URL_PREFIX):
+                # decode parameter data from dataurl
+                all_bytes = base64.urlsafe_b64decode(raw[len(DATA_URL_PREFIX):])
+                t, data_bytes = T.unflatten(all_bytes, 'ss')
+                data = T.unflatten(data_bytes, t)
+            else:
+                # old parameters may have been saved using repr
+                data = T.evalLRData(raw)
+            return dict(label=label, data=data)
+        count = S.getint(gen, 'Parameters')
+        self.parameters = [getPar(i) for i in range(count)]
+
+        # get comments if they're there
+        if S.has_section('Comments'):
+            def getComment(i):
+                sec = 'Comments'
+                time, user, comment = eval(S.get(sec, 'c%d' % i, raw=True))
+                return time_from_str(time), user, comment
+            count = S.getint(gen, 'Comments')
+            self.comments = [getComment(i) for i in range(count)]
+        else:
+            self.comments = []
+
+    def save(self):
+        S = util.DVSafeConfigParser()
+
+        sec = 'General'
+        S.add_section(sec)
+        S.set(sec, 'Created',  time_to_str(self.created))
+        S.set(sec, 'Accessed', time_to_str(self.accessed))
+        S.set(sec, 'Modified', time_to_str(self.modified))
+        S.set(sec, 'Title',       self.title)
+        S.set(sec, 'Independent', repr(len(self.independents)))
+        S.set(sec, 'Dependent',   repr(len(self.dependents)))
+        S.set(sec, 'Parameters',  repr(len(self.parameters)))
+        S.set(sec, 'Comments',    repr(len(self.comments)))
+
+        for i, ind in enumerate(self.independents):
+            sec = 'Independent %d' % (i+1)
+            S.add_section(sec)
+            S.set(sec, 'Label', ind['label'])
+            S.set(sec, 'Units', ind['units'])
+
+        for i, dep in enumerate(self.dependents):
+            sec = 'Dependent %d' % (i+1)
+            S.add_section(sec)
+            S.set(sec, 'Label',    dep['label'])
+            S.set(sec, 'Units',    dep['units'])
+            S.set(sec, 'Category', dep['category'])
+
+        for i, par in enumerate(self.parameters):
+            sec = 'Parameter %d' % (i+1)
+            S.add_section(sec)
+            S.set(sec, 'Label', par['label'])
+            # encode the parameter value as a data-url
+            data_bytes, t = T.flatten(par['data'])
+            all_bytes, _ = T.flatten((str(t), data_bytes), 'ss')
+            data_url = DATA_URL_PREFIX + base64.urlsafe_b64encode(all_bytes)
+            S.set(sec, 'Data', data_url)
+
+        sec = 'Comments'
+        S.add_section(sec)
+        for i, (time, user, comment) in enumerate(self.comments):
+            time = time_to_str(time)
+            S.set(sec, 'c%d' % i, repr((time, user, comment)))
+
+        with open(self.infofile, 'w') as f:
+            S.write(f)
+
+    def initialize_info(self, title, indep, dep):
+        self.title = title
+        self.accessed = self.modified = self.created = datetime.datetime.now()
+        self.independents = indep
+        self.dependents = dep
+        self.parameters = []
+        self.comments = []
+        self.cols = len(indep) + len(dep)
+
+    def access(self):
+        self.accessed = datetime.datetime.now()
+
+    def getIndependents(self):
+        return [(i['label'], i['units']) for i in self.independents]
+
+    def getDependents(self):
+        return [(d['category'], d['label'], d['units']) for d in self.dependents]
+
+    def addParam(self, name, data):
+        for p in self.parameters:
+            if p['label'] == name:
+                raise errors.ParameterInUseError(name)
+        d = dict(label=name, data=data)
+        self.parameters.append(d)
+    
+    def getParameter(self, name, case_sensitive=True):
+        for p in self.parameters:
+            if case_sensitive:
+                if p['label'] == name:
+                    return p['data']
+            else:
+                if p['label'].lower() == name.lower():
+                    return p['data']
+        raise errors.BadParameterError(name)
+    
+    def getParamNames(self):
+        return [ p['label']  for p in self.parameters ]
+
+    def addComment(self, user, comment):
+        self.comments.append((datetime.datetime.now(), user, comment))
+
+    def getComments(self, limit, start):
+        if limit is None:
+            comments = self.comments[start:]
+        else:
+            comments = self.comments[start:start+limit]
+        return comments, start + len(comments)
+
+    def numComments(self):
+        return len(self.comments)
+
+class CsvListData(IniData):
     """
     Data backed by a csv-formatted file.
 
     Stores the entire contents of the file in memory as a list or numpy array
     """
 
-    def __init__(self, filename, cols, file_timeout=FILE_TIMEOUT, data_timeout=DATA_TIMEOUT):
+    def __init__(self, filename, file_timeout=FILE_TIMEOUT, data_timeout=DATA_TIMEOUT):
         self.filename = filename
-        self._file = SelfClosingFile(filename, 'a+', timeout=file_timeout)
-        self.cols = cols
+        self._file = SelfClosingFile(open_args=(filename, 'a+'), timeout=file_timeout)
         self.timeout = data_timeout
+        self.infofile = filename[:-4] + '.ini'
 
     @property
     def file(self):
@@ -102,7 +277,7 @@ class CsvListData(object):
         if not len(data) or not isinstance(data[0], list):
             data = [data]
         if len(data[0]) != self.cols:
-            raise BadDataError(self.cols, len(data[0]))
+            raise errors.BadDataError(self.cols, len(data[0]))
 
         # append the data to the file
         self._saveData(data)
@@ -124,10 +299,10 @@ class CsvNumpyData(CsvListData):
     Stores the entire contents of the file in memory as a list or numpy array
     """
 
-    def __init__(self, filename, cols):
+    def __init__(self, filename):
         self.filename = filename
-        self._file = SelfClosingFile(filename, 'a+')
-        self.cols = cols
+        self._file = SelfClosingFile(open_args=(filename, 'a+'))
+        self.infofile = filename[:-4] + '.ini'
 
     @property
     def file(self):
@@ -187,7 +362,7 @@ class CsvNumpyData(CsvListData):
 
         # check row length
         if data.shape[-1] != self.cols:
-            raise BadDataError(self.cols, data.shape[-1])
+            raise errors.BadDataError(self.cols, data.shape[-1])
 
         # append data to in-memory data
         if self.data.size > 0:
@@ -216,7 +391,7 @@ class CsvNumpyData(CsvListData):
             nrows = len(self.data) if self.data.size > 0 else 0
             return pos < nrows
 
-def create_backend(filename, cols):
+def create_backend(filename):
     """Make a data object that manages in-memory and on-disk storage for a dataset.
 
     filename should be specified without a file extension. If there is an existing
@@ -226,11 +401,11 @@ def create_backend(filename, cols):
     csv_file = filename + '.csv'
     if os.path.exists(csv_file):
         if use_numpy:
-            return CsvNumpyData(csv_file, cols)
+            return CsvNumpyData(csv_file)
         else:
-            return CsvListData(csv_file, cols)
+            return CsvListData(csv_file)
     else:
         if use_numpy:
-            return CsvNumpyData(csv_file, cols)
+            return CsvNumpyData(csv_file)
         else:
-            return CsvListData(csv_file, cols)
+            return CsvListData(csv_file)
