03482cb24e6ba8b27ac319f8d1ed49b76afde0b9
joshmutus <josh.mutus@gmail.com>
2015-05-01 20:30:52 -0700

    Cleaned up d3_grapher so it loads properly with the http_server. Commented out some of the Oath2 code and dependencies since I'm just trying to get the grapher to work correctly for now. Sample grapher loads test_data and plots using dynamic plots.

--- http/modules/d3_grapher.py
+++ http/modules/d3_grapher.py
@@ -2,18 +2,16 @@ __author__ = 'mutus'
 from twisted.web.template import Element,XMLFile,renderer,flattenString
 import numpy as np
 import json
+from http_server import render_safe
 
-class Create1DTestData:
-
-    def __init__(self,len=100):
+def create1DTestData(len=100):
     '''
     Creates Line plots with "len" points, both a ramp and a random value
     '''
-        x_axis = np.arange(len).reshape(len,1)
-        rand_y = np.random.randint(len, size=(len,1))
-        ramp_y = np.vstack([np.arange(len/2),np.arange(len/2,0,-1)]).reshape(len,1)
-        self.rand_data = np.hstack([x_axis,rand_y])
-        self.ramp_data = np.hstack([x_axis,ramp_y])
+    x_axis = np.arange(len).reshape(len,1)
+    rand_y = np.random.randint(len, size=(len,1))
+    ramp_y = np.vstack([np.arange(len/2),np.arange(len/2,0,-1)]).reshape(len,1)
+    return  np.hstack([x_axis,rand_y]), np.hstack([x_axis,ramp_y])
 
 def create_2d_test_data(indep= 500,dep =300):
     '''
@@ -32,20 +30,21 @@ def create_2d_test_data(indep= 500,dep =300):
     deps = np.random.randint(256,size=(foo_arr.shape[0],3))
     return np.hstack([foo_arr,deps])
 
-class PlotPage(Element):
-    def __init__(self):
-        pass
+class D3GrapherPage(Element):
     loader = XMLFile('modules/d3_grapher.xml')
 
-    @renderer
+    def __init__(self, cxn, request):
+        super(D3GrapherPage, self).__init__()
+
+    @render_safe
     def header(self, request, tag):
         return tag('Header.')
 
-    @renderer
+    @render_safe
     def footer(self, request, tag):
         return tag('Footer.')
 
-    @renderer
+    @render_safe
     def plot_style(self,request,tag):
         '''
         This will eventually be used to set up the style of the plots
@@ -55,7 +54,7 @@ class PlotPage(Element):
         '''
         return tag('CSS GOES HERE')
 
-    @renderer
+    @render_safe
     def plot(self,request, tag):
         '''
         This fills d3.js pan-able zoomable plot
@@ -63,15 +62,13 @@ class PlotPage(Element):
         :param tag:
         :return:
         '''
-        d = 1 #2 is 2D plot, 1 is 1D plot
+        d = 2 #2 is 2D plot, 1 is 1D plot
         x_min,x_max= 0,100
         y_min,y_max= 0,100
         x_label, y_label = "Independent","Dependent"
         plot_extents = "xMin: %d, xMax: %d, yMin:%d, yMax:%d"%(x_min,x_max,y_min,y_max)
         if d==1:
-            test_1D = Create1DTestData(len=100)
-            test_data = test_1D.ramp_data.tolist()
-            test_data2 = test_1D.rand_data.tolist()
+            test_data, test_data2 = create1DTestData(len=100)
             #I know this is inelegant, but I'm passing zeroes to the frontend for variables that just aren't needed
             # I'm sure there's a better way
             dep_len = 0
@@ -79,16 +76,15 @@ class PlotPage(Element):
             plot_len_2D = "indep_len: %d, dep_len: %d"%(indep_len,dep_len)
 
         if d==2:
-            dep_len = 5
-            indep_len = 3
-            test_data =  create_2d_test_data(indep=indep_len,dep=dep_len).tolist()
+            dep_len = 10
+            indep_len = 20
+            test_data =  create_2d_test_data(indep=indep_len,dep=dep_len)
             print test_data
-            test_1D = Create1DTestData(len=100)
-            test_data2 = test_1D.rand_data.tolist()
+            test_1D, test_data2 = create1DTestData(len=100)
             plot_len_2D = "indep_len: %d, dep_len: %d"%(indep_len,dep_len)
 
 
-        yield (tag.clone().fillSlots(data_ent = json.dumps(test_data),data_ent2 = json.dumps(test_data2),
+        yield (tag.clone().fillSlots(data_ent = json.dumps(test_data.tolist()),data_ent2 = json.dumps(test_data2.tolist()),
                                     plot_extents = plot_extents,
                                     x_label = x_label,
                                     y_label = y_label,
@@ -96,5 +92,12 @@ class PlotPage(Element):
                                     plot_dim = str(d)
         ))
 
+class D3GrapherFuncs():
+    '''
+    This is the class that handles the backend functions
+    '''
+    def __init__(self):
+        pass
 
-
+page_factory = D3GrapherPage
+page_funcs = D3GrapherFuncs()
