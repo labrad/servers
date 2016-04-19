7206072a748ff4f0c9fe9522804efdcdf7504d33
Matthew Neeley <maffoo@google.com>
2016-04-19 15:28:35 -0700

    Rename some variables

--- datavault/backend.py
+++ datavault/backend.py
@@ -43,12 +43,12 @@ def labrad_urlencode(data):
     if hasattr(T, 'FlatData'):
         # pylabrad 0.95+
         flat_data = T.flatten(data)
-        flat_bytes = T.flatten((str(flat_data.tag), flat_data.bytes), 'sy')
-        bytes = flat_bytes.bytes
+        flat_cluster = T.flatten((str(flat_data.tag), flat_data.bytes), 'sy')
+        all_bytes = flat_cluster.bytes
     else:
         data_bytes, t = T.flatten(data)
-        bytes, _ = T.flatten((str(t), data_bytes), 'ss')
-    data_url = DATA_URL_PREFIX + base64.urlsafe_b64encode(bytes)
+        all_bytes, _ = T.flatten((str(t), data_bytes), 'ss')
+    data_url = DATA_URL_PREFIX + base64.urlsafe_b64encode(all_bytes)
     return data_url
 
 def labrad_urldecode(data_url):
