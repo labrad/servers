a5c44e2cd2142f45ce7e01c2484e8d063bced343
martinisgroup <scQubitsUCSB@gmail.com>
2016-05-16 10:42:58 -0700

    Adds voltage unit values sent to to set voltage functions
    OTS commit with Ben Chiaro

--- GUIs/CurrentBias/currentBiasSerial.py
+++ GUIs/CurrentBias/currentBiasSerial.py
@@ -1,6 +1,7 @@
 import sys
 import labrad
 from PyQt4 import QtCore,QtGui,uic
+from labrad.units import V
 
 with labrad.connect() as cxn:
     dc = cxn.dc_rack_server
@@ -47,38 +48,38 @@ with labrad.connect() as cxn:
             print self.ui.fineCoarseSelectA.sliderPosition()
             if self.ui.fineCoarseSelectA.sliderPosition():
                 print 'coarse'
-                self.setvoltage('A',1L,self.ui.fastSlowSelectA.sliderPosition(),setCoarseA)
+                self.setvoltage('A',1L,self.ui.fastSlowSelectA.sliderPosition(),setCoarseA*V)
             else:
                 print 'fine'
-                self.setvoltage('A',0L,self.ui.fastSlowSelectA.sliderPosition(),setFineA)
+                self.setvoltage('A',0L,self.ui.fastSlowSelectA.sliderPosition(),setFineA*V)
             
         @QtCore.pyqtSlot()
         def on_fineSliderA_sliderReleased(self):
             self.ui.fineBoxA.setValue(self.ui.fineSliderA.sliderPosition())
             setFineA = float(self.ui.fineSliderA.sliderPosition())/1000.0
             if not self.ui.fineCoarseSelectA.sliderPosition():
-                self.setvoltage('A',0L,self.ui.fastSlowSelectA.sliderPosition(),setFineA)
+                self.setvoltage('A',0L,self.ui.fastSlowSelectA.sliderPosition(),setFineA*V)
             
         @QtCore.pyqtSlot()
         def on_coarseSliderA_sliderReleased(self):
             self.ui.coarseBoxA.setValue(self.ui.coarseSliderA.sliderPosition())
             setCoarseA = float(self.ui.coarseSliderA.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectA.sliderPosition():
-                self.setvoltage('A',1L,self.ui.fastSlowSelectA.sliderPosition(),setCoarseA)   
+                self.setvoltage('A',1L,self.ui.fastSlowSelectA.sliderPosition(),setCoarseA*V)   
         
         @QtCore.pyqtSlot(int)
         def on_fineBoxA_valueChanged(self, value):
             self.ui.fineSliderA.setValue(value)
             setFineA = float(value)/1000.0
             if not self.ui.fineCoarseSelectA.sliderPosition():
-                self.setvoltage('A',0L,self.ui.fastSlowSelectA.sliderPosition(),setFineA) 
+                self.setvoltage('A',0L,self.ui.fastSlowSelectA.sliderPosition(),setFineA*V) 
 
         @QtCore.pyqtSlot(int)
         def on_coarseBoxA_valueChanged(self, value):
             self.ui.coarseSliderA.setValue(self.ui.coarseBoxA.value())
             setCoarseA = float(value)/1000.0
             if self.ui.fineCoarseSelectA.sliderPosition():
-                self.setvoltage('A',1L,self.ui.fastSlowSelectA.sliderPosition(),setCoarseA)                    
+                self.setvoltage('A',1L,self.ui.fastSlowSelectA.sliderPosition(),setCoarseA*V)                    
                 
         @QtCore.pyqtSlot()
         def on_pushButtonA_clicked(self):
@@ -90,37 +91,37 @@ with labrad.connect() as cxn:
             setFineB = float(self.ui.fineSliderB.sliderPosition())/1000.0
             setCoarseB = float(self.ui.coarseSliderB.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectB.sliderPosition():
-                self.setvoltage('B',1L,self.ui.fastSlowSelectB.sliderPosition(),setCoarseB)
+                self.setvoltage('B',1L,self.ui.fastSlowSelectB.sliderPosition(),setCoarseB*V)
             else:
-                self.setvoltage('B',0L,self.ui.fastSlowSelectB.sliderPosition(),setFineB)
+                self.setvoltage('B',0L,self.ui.fastSlowSelectB.sliderPosition(),setFineB*V)
             
         @QtCore.pyqtSlot()
         def on_fineSliderB_sliderReleased(self):
             self.ui.fineBoxB.setValue(self.ui.fineSliderB.sliderPosition())
             setFineB = float(self.ui.fineSliderB.sliderPosition())/1000.0
             if not self.ui.fineCoarseSelectB.sliderPosition():
-                self.setvoltage('B',0L,self.ui.fastSlowSelectB.sliderPosition(),setFineB)
+                self.setvoltage('B',0L,self.ui.fastSlowSelectB.sliderPosition(),setFineB*V)
             
         @QtCore.pyqtSlot()
         def on_coarseSliderB_sliderReleased(self):
             self.ui.coarseBoxB.setValue(self.ui.coarseSliderB.sliderPosition())
             setCoarseB = float(self.ui.coarseSliderB.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectB.sliderPosition():
-                self.setvoltage('B',1L,self.ui.fastSlowSelectB.sliderPosition(),setCoarseB) 
+                self.setvoltage('B',1L,self.ui.fastSlowSelectB.sliderPosition(),setCoarseB*V) 
         
         @QtCore.pyqtSlot(int)
         def on_fineBoxB_valueChanged(self, value):
             self.ui.fineSliderB.setValue(value)
             setFineB = float(value)/1000.0
             if not self.ui.fineCoarseSelectB.sliderPosition():
-                self.setvoltage('B',0L,self.ui.fastSlowSelectB.sliderPosition(),setFineB) 
+                self.setvoltage('B',0L,self.ui.fastSlowSelectB.sliderPosition(),setFineB*V) 
 
         @QtCore.pyqtSlot(int)
         def on_coarseBoxB_valueChanged(self, value):
             self.ui.coarseSliderB.setValue(self.ui.coarseBoxD.value())
             setCoarseB = float(value)/1000.0
             if self.ui.fineCoarseSelectB.sliderPosition():
-                self.setvoltage('B',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseB)         
+                self.setvoltage('B',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseB*V)         
                 
         @QtCore.pyqtSlot()
         def on_pushButtonB_clicked(self):
@@ -132,16 +133,16 @@ with labrad.connect() as cxn:
             setFineC = float(self.ui.fineSliderC.sliderPosition())/1000.0
             setCoarseC = float(self.ui.coarseSliderC.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectC.sliderPosition():
-                self.setvoltage('C',1L,self.ui.fastSlowSelectC.sliderPosition(),setCoarseC)
+                self.setvoltage('C',1L,self.ui.fastSlowSelectC.sliderPosition(),setCoarseC*V)
             else:
-                self.setvoltage('C',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineC)
+                self.setvoltage('C',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineC*V)
             
         @QtCore.pyqtSlot()
         def on_fineSliderC_sliderReleased(self):
             self.ui.fineBoxC.setValue(self.ui.fineSliderC.sliderPosition())
             setFineC = float(self.ui.fineSliderC.sliderPosition())/1000.0
             if not self.ui.fineCoarseSelectC.sliderPosition():
-                self.setvoltage('C',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineC)
+                self.setvoltage('C',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineC*V)
                 print 'fine'
             
         @QtCore.pyqtSlot()
@@ -149,21 +150,21 @@ with labrad.connect() as cxn:
             self.ui.coarseBoxC.setValue(self.ui.coarseSliderC.sliderPosition())
             setCoarseC = float(self.ui.coarseSliderC.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectC.sliderPosition():
-                self.setvoltage('C',1L,self.ui.fastSlowSelectC.sliderPosition(),setCoarseC)        
+                self.setvoltage('C',1L,self.ui.fastSlowSelectC.sliderPosition(),setCoarseC*V)        
   
         @QtCore.pyqtSlot(int)
         def on_fineBoxC_valueChanged(self, value):
             self.ui.fineSliderC.setValue(value)
             setFineC = float(value)/1000.0
             if not self.ui.fineCoarseSelectC.sliderPosition():
-                self.setvoltage('C',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineC) 
+                self.setvoltage('C',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineC*V) 
 
         @QtCore.pyqtSlot(int)
         def on_coarseBoxC_valueChanged(self, value):
             self.ui.coarseSliderC.setValue(self.ui.coarseBoxC.value())
             setCoarseC = float(value)/1000.0
             if self.ui.fineCoarseSelectC.sliderPosition():
-                self.setvoltage('C',1L,self.ui.fastSlowSelectC.sliderPosition(),setCoarseC)        
+                self.setvoltage('C',1L,self.ui.fastSlowSelectC.sliderPosition(),setCoarseC*V)        
 
         @QtCore.pyqtSlot()
         def on_pushButtonC_clicked(self):
@@ -175,37 +176,37 @@ with labrad.connect() as cxn:
             setFineD = float(self.ui.fineSliderD.sliderPosition())/1000.0
             setCoarseD = float(self.ui.coarseSliderD.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectD.sliderPosition():
-                self.setvoltage('D',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseD)
+                self.setvoltage('D',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseD*V)
             else:
-                self.setvoltage('D',0L,self.ui.fastSlowSelectD.sliderPosition(),setFineD)
+                self.setvoltage('D',0L,self.ui.fastSlowSelectD.sliderPosition(),setFineD*V)
             
         @QtCore.pyqtSlot()
         def on_fineSliderD_sliderReleased(self):
             self.ui.fineBoxD.setValue(self.ui.fineSliderD.sliderPosition())
             setFineD = float(self.ui.fineSliderD.sliderPosition())/1000.0
             if not self.ui.fineCoarseSelectD.sliderPosition():
-                self.setvoltage('D',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineD)
+                self.setvoltage('D',0L,self.ui.fastSlowSelectC.sliderPosition(),setFineD*V)
             
         @QtCore.pyqtSlot()
         def on_coarseSliderD_sliderReleased(self):
             self.ui.coarseBoxD.setValue(self.ui.coarseSliderD.sliderPosition())
             setCoarseD = float(self.ui.coarseSliderD.sliderPosition())/1000.0
             if self.ui.fineCoarseSelectD.sliderPosition():
-                self.setvoltage('D',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseD)      
+                self.setvoltage('D',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseD*V)      
 
         @QtCore.pyqtSlot(int)
         def on_fineBoxD_valueChanged(self, value):
             self.ui.fineSliderD.setValue(value)
             setFineD = float(value)/1000.0
             if not self.ui.fineCoarseSelectD.sliderPosition():
-                self.setvoltage('D',0L,self.ui.fastSlowSelectD.sliderPosition(),setFineD) 
+                self.setvoltage('D',0L,self.ui.fastSlowSelectD.sliderPosition(),setFineD*V) 
 
         @QtCore.pyqtSlot(int)
         def on_coarseBoxD_valueChanged(self, value):
             self.ui.coarseSliderD.setValue(self.ui.coarseBoxD.value())
             setCoarseD = float(value)/1000.0
             if self.ui.fineCoarseSelectD.sliderPosition():
-                self.setvoltage('D',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseD)          
+                self.setvoltage('D',1L,self.ui.fastSlowSelectD.sliderPosition(),setCoarseD*V)          
         
         @QtCore.pyqtSlot()
         def on_pushButtonD_clicked(self):
