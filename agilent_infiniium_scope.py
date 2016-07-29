bc41ff3df6b6adaeede34f00259e716e5f491232
Brooks Campbell <brooksrcampbell@gmail.com>
2016-07-29 16:18:21 -0700

    Print only first byte of data in exception.

--- agilent_infiniium_scope.py
+++ agilent_infiniium_scope.py
@@ -356,7 +356,8 @@ def _parseBinaryData(data, word_length):
     # Get rid of header
     # unpack binary data
     if data[0] != '#':
-        raise Exception("Invalid wave data. Expected '#' at start. {}".format(data))
+        raise Exception("Invalid wave data. Expected '#' "
+                        "at start. {}".format(data[0]))
     len_header = int(data[1])
     wave_offset = 2 + len_header
     len_wave = int(data[2:wave_offset])
